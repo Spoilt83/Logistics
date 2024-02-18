faa542a20008b93622ac377e43d616ceb4e57f5f
Marvin <marvinsolano83@gmail.com>
2024-02-18 13:11:07 -0500

    adding models and configuring create, update and delete resources

--- logisticSystem/logistics/urls.py
+++ logisticSystem/logistics/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create_shipment', views.create_shipment, name='create_shipment'),
+    path('update_shipment', views.update_shipment, name='update_shipment'),
+    path('delete_shipment', views.delete_shipment, name='delete_shipment'),
     path('packages', views.packages, name='packages'),
     path('packages_sent', views.packages_sent, name='packages_sent'),
     path('packages_asigned', views.packages_asigned, name='packages_asigned'),
--- logisticSystem/logistics/views.py
+++ logisticSystem/logistics/views.py
@@ -10,6 +10,18 @@ def packages(request):
     return render(request, 'packages/packages.html')
 
 
+def create_shipment(request):
+    return render(request, 'packages/create_shipment.html')
+
+
+def update_shipment(request):
+    return render(request, 'packages/update_shipment.html')
+
+
+def delete_shipment(request):
+    return render(request, 'packages/delete_shipment.html')
+
+
 def packages_sent(request):
     return render(request, 'pages/packages_sent.html')
 
